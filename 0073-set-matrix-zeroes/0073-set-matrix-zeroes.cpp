8aeaefcbfeae6a1a5bd41a598f4ad76b225a9ac1
priyanka5200 <105165521+priyanka5200@users.noreply.github.com>
2026-08-15 12:32:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
+    void markRow(vector<vector<int>>& matrix, int n, int m, int i)
+    {
+        for(int j=0; j<m; j++)
+        {
+            if(matrix[i][j] != 0)
+            {
+                matrix[i][j] = '#';
+            }
+        }
+    }
+     void markCol(vector<vector<int>>& matrix, int n,int m, int j)
+    {
+        for(int i=0; i<n;i++)
+        {
+            if(matrix[i][j] != 0)
+            {
+                matrix[i][j] = '#';
+            }
+        }
+    }
     void setZeroes(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int col = matrix[0].size();
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        vector<int> dummy1(row, -1);
-        vector<int> dummy2(col, -1);
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (matrix[i][j] == 0) {
-                    dummy1[i] = 0;
-                    dummy2[j] = 0;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    markRow(matrix, n,m, i);
+                    markCol(matrix, n,m, j);
                 }
             }
         }
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (dummy1[i] == 0 || dummy2[j] == 0) {
-                    matrix[i][j] = 0;
-                }
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<m; j++)
+            {
+               if( matrix[i][j] == '#');
+               matrix[i][j] = 0;
             }
         }
+        
     }
 };
