35dbe0472198250f8e5527ebc2ad987232cefab3
priyanka5200 <105165521+priyanka5200@users.noreply.github.com>
2026-08-26 13:35:39 +0530

    Time: 26 ms (62.95%), Space: 16.2 MB (90.35%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,24 +1,20 @@
 class Solution {
-public:
-    bool isValid(int i, int j, int n, int m, vector<vector<char>>& grid) {
-        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') {
-            return false;
-        }
-        return true;
-    }
 
 public:
     void dfs(int i, int j, int n, int m, vector<vector<char>>& grid) {
+
+        if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] != '1') {
+            return;
+        }
         grid[i][j] = '0';
 
-        if (isValid(i + 1, j, n, m, grid))
-            dfs(i + 1, j, n, m, grid);
-        if (isValid(i - 1, j, n, m, grid))
-            dfs(i - 1, j, n, m, grid);
-        if (isValid(i, j + 1, n, m, grid))
-            dfs(i, j + 1, n, m, grid);
-        if (isValid(i, j - 1, n, m, grid))
-            dfs(i, j - 1, n, m, grid);
+        dfs(i + 1, j, n, m, grid);
+
+        dfs(i - 1, j, n, m, grid);
+
+        dfs(i, j + 1, n, m, grid);
+
+        dfs(i, j - 1, n, m, grid);
     }
 
 public:
