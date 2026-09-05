7aa9a1870b61774879b736e111a2c97daa9b228e
priyanka5200 <105165521+priyanka5200@users.noreply.github.com>
2026-09-05 11:34:16 +0530

    Time: 0 ms (100%), Space: 8.2 MB (23.35%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if (s.size() != goal.size()) {
+        if(s.size() != goal.size())
+        {
             return false;
         }
-        s = s + s;
-
-        for (int i = 0; i < goal.size(); i++) {
-
-            string str = s.substr(i, goal.size());
-            if (str == goal) {
+        string str = s+s;
+        for(int i=0; i<goal.size(); i++)
+        {
+            string temp = str.substr(i,goal.size());
+            if(temp == goal)
+            {
                 return true;
             }
         }
         return false;
+        
     }
 };
