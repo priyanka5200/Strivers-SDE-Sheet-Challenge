5a8d133408208709979cb3280c27e7ed9d3e69ae
priyanka5200 <105165521+priyanka5200@users.noreply.github.com>
2026-09-05 11:41:37 +0530

    Time: 7 ms (18.23%), Space: 9.7 MB (75.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) {
-            return 0;
-        }
-
-        vector<int> freq(26, 0);
-
-        for (int i = 0; i < s.size(); i++) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
-        }
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] > 0) {
-                return 0;
-            }
-        }
-        return 1;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
+        
     }
 };
