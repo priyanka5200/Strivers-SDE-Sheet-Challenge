5543c518c8941a8f193f8f70e0a34f85a1ebd315
priyanka5200 <105165521+priyanka5200@users.noreply.github.com>
2026-09-20 22:44:28 +0530

    Time: 0 ms (100%), Space: 7.8 MB (97.83%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,35 @@
 class Solution {
-    int fun(int i, int j, vector<vector<int>>&dp)
-    {
-        if(i==0 && j==0)return 1;
-        if(i<0 || j<0)return 0;
-        if(dp[i][j] != -1)return dp[i][j];
-        int left = fun(i,j-1,dp);
-        int up = fun(i-1, j,dp);
-        return dp[i][j] = up+left;
-    }
+    // int fun(int i, int j, vector<vector<int>>&dp)
+    // {
+    //     if(i==0 && j==0)return 1;
+    //     if(i<0 || j<0)return 0;
+    //     if(dp[i][j] != -1)return dp[i][j];
+    //     int left = fun(i,j-1,dp);
+    //     int up = fun(i-1, j,dp);
+    //     return dp[i][j] = up+left;
+    // }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n,-1));
-      return fun(m-1,n-1,dp);
+      int dp[m][n];
+      //dp[0][0] = 1;
+      int up, left;
+
+      for(int i=0; i<m; i++)
+      {
+        for(int j=0; j<n; j++)
+        {
+            if(i==0&&j==0)dp[i][j] = 1;
+            else{
+            up=0;
+            left=0;
+           if(i>0)up = dp[i-1][j];
+           if(j>0)left = dp[i][j-1];
+           dp[i][j] = up+left;
+            }
+        }
+      }
+      return dp[m-1][n-1];
+     
         
     }
 };
