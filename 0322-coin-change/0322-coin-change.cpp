7e6f3bb552b0cac4dbbefaf39dce7d20a22064fb
priyanka5200 <105165521+priyanka5200@users.noreply.github.com>
2026-09-26 12:43:14 +0530

    Time: 39 ms (46.76%), Space: 48.7 MB (43.3%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
-    int f(int ind, int T, vector<int>coins,  vector<vector<int>>&dp)
-    {
-        if(ind ==0)
-        {
-            if(T%coins[ind] == 0)return T/coins[ind];
-            else
-            return 1e9;
-        }
-        if(dp[ind][T] != -1)return dp[ind][T];
-        int not_pick = f(ind-1, T, coins,dp);
-        int pick = 1e9;
-        if(coins[ind] <= T)
-        pick = 1+f(ind, T-coins[ind], coins,dp);
-        return dp[ind][T] =  min(pick, not_pick);
-    }
+    // int f(int ind, int T, vector<int>coins,  vector<vector<int>>&dp)
+    // {
+    //     if(ind ==0)
+    //     {
+    //         if(T%coins[0] == 0)return T/coins[0];
+    //         else
+    //         return 1e9;
+    //     }
+    //     if(dp[ind][T] != -1)return dp[ind][T];
+    //     int not_pick = f(ind-1, T, coins,dp);
+    //     int pick = 1e9;
+    //     if(coins[ind] <= T)
+    //     pick = 1+f(ind, T-coins[ind], coins,dp);
+    //     return dp[ind][T] =  min(pick, not_pick);
+    // }
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n, vector<int>(amount+1, -1));
-        int ans = f(n-1, amount, coins, dp);
-        if(ans >= 1e9)return -1;
+        vector<vector<int>>dp(n, vector<int>(amount+1, 0));
+        for(int t=0; t<=amount; t++)
+        {
+            if(t%coins[0]==0)dp[0][t]=t/coins[0];
+            else
+            dp[0][t] = 1e9;
+        }
+        for(int ind=1; ind<n; ind++)
+        {
+            for(int t=0; t<=amount; t++)
+            {
+                int not_pick = dp[ind-1][t];
+                int pick = 1e9;
+                if(coins[ind] <= t)
+                {
+                    pick = 1+dp[ind][t-coins[ind]];
+                }
+                dp[ind][t] = min(pick, not_pick);
+            }
+        }
+        if(dp[n-1][amount]>=1e9)return -1;
         else
-        return ans;
+        return dp[n-1][amount];
         
     }
 };
